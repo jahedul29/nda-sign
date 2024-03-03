567ceea1fa6e914c53e5f252e97a9287d791538e
jahedul29 <jahedulh1@gmail.com>
2024-03-03 07:23:20 +0100

    sign up page validation done

---
+++ src/form-schema/authSchema.js
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+const emailRegexPattern =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const strongPasswordRegexPattern =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+export const signupFormSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .matches(emailRegexPattern, "Please enter a valid email address"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .matches(
+      strongPasswordRegexPattern,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+  confirmPassword: yup
+    .string()
+    .required("Confirm password is required")
+    .oneOf([yup.ref("password"), null], "Password must match"),
+});
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,34 +2,47 @@ import React from 'react'
 import './style.css';
 import FormInput from '../../components/form/formInput/formInput';
 import { Link } from 'react-router-dom';
+import Form from '../../components/form/Form';
+import { signupFormSchema } from '../../form-schema/authSchema';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 export const Signup = () => {
+
+  const handleSignup = async data => {
+    console.log({ data })
+  }
+
   return (
     <div className='auth-container'>
       <div className="content">
         <h1>Sign the documents</h1>
         <div className='inner-content'>
           <h1>Create your account</h1>
-          <div className='input-container'>
-            <FormInput label={"Name"} placeholder={"Enter your name"} type={'text'} onChange={(e) => console.log(e.target.value)}/>
-          </div>
-          <div className='input-container'>
-            <FormInput label={"Email"} placeholder={"Enter email"} type={'text'} onChange={(e) => console.log(e.target.value)}/>
-          </div>
-          <div className='input-container'>
-            <FormInput label={"Password"} placeholder={"Password"} type={'password'} onChange={(e) => console.log(e.target.value)}/>
-          </div>
-          <div className='input-container'>
-            <FormInput label={"Confirm password"} placeholder={"Password"} type={'password'} onChange={(e) => console.log(e.target.value)}/>
-          </div>
-          <div className='input-container'>
-           <button type='submit' className='auth-button'>Sign Up</button>
-          </div>
-          <div  className="agree-terms">
-            <label htmlFor='agree'>
-              <input type='checkbox' name='agree' id='agree' /> <p>I agree to all Term, Privacy Policy and Fees</p> 
-            </label>
-          </div>
+          <Form
+            resolver={yupResolver(signupFormSchema)}
+            submitHandler={handleSignup}
+          >
+            <div className='input-container'>
+              <FormInput name={'name'} label={"Name"} placeholder={"Enter your name"} type={'text'} onChange={(e) => console.log(e.target.value)}/>
+            </div>
+            <div className='input-container'>
+              <FormInput name={'email'} label={"Email"} placeholder={"Enter email"} type={'text'} onChange={(e) => console.log(e.target.value)}/>
+            </div>
+            <div className='input-container'>
+              <FormInput name={'password'} label={"Password"} placeholder={"Password"} type={'password'} onChange={(e) => console.log(e.target.value)}/>
+            </div>
+            <div className='input-container'>
+              <FormInput name={'confirmPassword'} label={"Confirm password"} placeholder={"Password"} type={'password'} onChange={(e) => console.log(e.target.value)}/>
+            </div>
+            <div  className="agree-terms">
+              <label htmlFor='agree'>
+                <input type='checkbox' name='agree' id='agree' /> <p>I agree to all Term, Privacy Policy and Fees</p> 
+              </label>
+            </div>
+            <div className='input-container'>
+              <button type='submit' className='auth-button'>Sign Up</button>
+            </div>
+          </Form>
           <div className="already-have-account">
             <p>Already have an account? <Link to="/login">Log in</Link></p>
           </div>
